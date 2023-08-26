0a009def819ca7af51d2ac21a56dd9c48f9402f9
백소원 <dnjs0236@gmail.com>
2023-08-27 01:09:16 +0900

    2023.08.27

---
+++ Baekjoon_4999.js
@@ -0,0 +1,8 @@
+const fs = require("fs")
+const input = fs.readFileSync("./input.txt").toString().trim().split("\n")
+
+const a = input[0]
+const b = input[1]
+
+const result = a.indexOf(b) >= 0 ? true : false // 문자열 포함여부 체크
+console.log(result ? "go" : "no")
